b61744413ebacffa07c26ee1ac9d9eaadcea4a64
Dagwaging <dagwaging@gmail.com>
2014-07-07 12:07:29 -0400

    Added player joining, formatted code

--- api.js
+++ api.js
@@ -1,59 +1,77 @@
 exports = module.exports = createApi;
 
-function createApi (db) {
+function createApi(db) {
     var self = this;
-    
+
     self.games = db.collection('games');
-    
+
     self.getGames = function(req, res) {
 	var players = req.query.players;
-	
+
 	var cursor;
-	
-	if(typeof players === 'undefined') {
+
+	if (typeof players === 'undefined') {
 	    cursor = self.games.find({});
-	}
-	else {
+	} else {
 	    var playerList = players.split(',');
-	    
+
 	    var playerQueries = [];
-	    
-	    for(var i in playerList) {
-		playerQueries[i] = {address:playerList[i]};
+
+	    for ( var i in playerList) {
+		playerQueries[i] = {
+		    address : playerList[i]
+		};
 	    }
-	    
-	    cursor = self.games.find({$or:playerQueries});
+
+	    cursor = self.games.find({
+		$or : playerQueries
+	    });
 	}
-	
+
 	cursor.toArray(function(err, docs) {
-	    res.send(docs);
+	    res.status(200).send(docs);
 	});
     };
-    
+
     self.createGame = function(req, res) {
 	var game = req.body;
-	
+
 	game.tags = [];
 	game.players = [];
-	
+
 	self.games.insert(game, function(err, inserted) {
-	    if(err)
+	    if (err)
 		console.log(err);
+	    else
+		res.status(201).send(game);
 	});
     };
-    
+
     self.getGame = function(req, res) {
 	req.params.id;
     };
-    
+
     self.tag = function(req, res) {
 	req.params.id;
     };
-    
+
     self.join = function(req, res) {
-	req.params.id;
+	var player = {
+	    address : req.body.address,
+	    pushId : req.body.pushId
+	};
+
+	self.games.update({
+	    _id : req.params.id
+	}, {
+	    $push : {
+		players : player
+	    }
+	});
+	
+	res.status(201).send(player);
     };
-    
+
     self.leave = function(req, res) {
 	req.params.id;
     };
